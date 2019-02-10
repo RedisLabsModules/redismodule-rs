d0a3909955f5997810e8e00e4cbfe01174249b79
Gavrie Philipson <gavrie@redislabs.com>
2019-02-10 18:36:43 +0200

    Map values with an iter

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,29 +1,37 @@
 use std::ffi::CString;
 
-#[macro_use]
+//#[macro_use]
 extern crate redismodule;
+
 use redismodule::{Context, Command, RedisResult, RedisValue, RedisError};
 
 fn hello_mul(_: &Context, args: Vec<String>) -> RedisResult {
-    if args.len() != 3 {
+    if args.len() < 3 {
         return Err(RedisError::WrongArity);
     }
 
-    let m1 = parse_integer(&args[1])?;
-    let m2 = parse_integer(&args[2])?;
+    let maybe_nums: Vec<_> = args
+        .into_iter()
+        .skip(1) // The command itself
+        .map(parse_integer)
+        .collect();
 
-    let result = m1 * m2;
+    let mut nums = vec![];
+    for n in maybe_nums {
+        nums.push(n?);
+    }
 
-    return Ok(RedisValue::Array(
-        vec![m1, m2, result]
-            .into_iter()
-            .map(|v| RedisValue::Integer(v))
-            .collect()));
+    nums.push(nums.iter().product());
+
+    return Ok(RedisValue::Array(nums
+        .into_iter()
+        .map(RedisValue::Integer)
+        .collect()));
 }
 
 //////////////////////////////////////////////////////
 
-fn parse_integer(arg: &str) -> Result<i64, RedisError> {
+fn parse_integer(arg: String) -> Result<i64, RedisError> {
     arg.parse::<i64>()
         .map_err(|_| RedisError::String(format!("Couldn't parse as integer: {}", arg)))
 }
@@ -33,8 +41,8 @@ fn parse_integer(arg: &str) -> Result<i64, RedisError> {
 const MODULE_NAME: &str = "hello";
 const MODULE_VERSION: u32 = 1;
 
+/*
 redis_module!(MODULE_NAME, MODULE_VERSION, [
     Command::new("hello.mul", hello_mul, "write"),
 ]);
-
-
+*/
