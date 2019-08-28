9fe30903685f5eca6ee746b6b3584083f9fb7c90
Gavrie Philipson <gavrie@redislabs.com>
2019-08-28 13:43:55 +0300

    Add support for converting Utf8Error to RedisError

--- src/rediserror.rs
+++ src/rediserror.rs
@@ -1,4 +1,6 @@
 use core::num::{ParseFloatError, ParseIntError};
+use std::str::Utf8Error;
+use std::string::FromUtf8Error;
 
 #[derive(Debug)]
 pub enum RedisError {
@@ -19,6 +21,12 @@ impl From<&'static str> for RedisError {
     }
 }
 
+impl From<String> for RedisError {
+    fn from(s: String) -> Self {
+        RedisError::String(s)
+    }
+}
+
 impl From<ParseFloatError> for RedisError {
     fn from(e: ParseFloatError) -> Self {
         RedisError::String(e.to_string())
@@ -27,6 +35,18 @@ impl From<ParseFloatError> for RedisError {
 
 impl From<ParseIntError> for RedisError {
     fn from(e: ParseIntError) -> Self {
+        RedisError::String(e.to_string())
+    }
+}
+
+impl From<FromUtf8Error> for RedisError {
+    fn from(e: FromUtf8Error) -> Self {
+        RedisError::String(e.to_string())
+    }
+}
+
+impl From<Utf8Error> for RedisError {
+    fn from(e: Utf8Error) -> Self {
         RedisError::String(e.to_string())
     }
 }
