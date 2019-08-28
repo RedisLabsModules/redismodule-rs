dc83cc9310a00af6b215c4031cfbe5d39c652b78
Gavrie Philipson <gavrie@redislabs.com>
2019-08-28 13:46:20 +0300

    Remove RediSearch support (moved to separate crate)

--- src/redisearch/redisearch_api.c
+++
@@ -1,8 +0,0 @@
-#include "redismodule.h"
-#include "redisearch_api.h"
-
-// We write a wrapper function since the actual implementation is a macro,
-// which we cannot call from Rust.
-int Wrap_RediSearch_Initialize() {
-    return RediSearch_Initialize();
-}
