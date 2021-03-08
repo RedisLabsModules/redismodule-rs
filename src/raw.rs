89780c20d309f06be9eeae9c19c540abd22332ce
Guy Korland <gkorland@gmail.com>
2021-03-08 15:24:16 +0200

    add support for notify_keyspace_event

--- src/raw.rs
+++ src/raw.rs
@@ -474,3 +474,10 @@ pub fn subscribe_to_server_event(
 ) -> Status {
     unsafe { RedisModule_SubscribeToServerEvent.unwrap()(ctx, event, callback).into() }
 }
+
+#[cfg(feature = "experimental-api")]
+pub fn notify_keyspace_event(ctx: *mut RedisModuleCtx, event_type: i32, event: &str, keyname: *mut RedisModuleString) {
+    let event = CString::new(event).unwrap();
+    unsafe { RedisModule_NotifyKeyspaceEvent.unwrap()(ctx, event_type, event.as_ptr(), keyname) };
+}
+
