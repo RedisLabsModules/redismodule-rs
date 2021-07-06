631652aabd57585726a505e0496ac8226990622f
Guy Korland <gkorland@gmail.com>
2021-07-06 17:23:47 +0300

    add context to errors

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,15 +1,21 @@
 mod utils;
 
+use anyhow::Context;
 use anyhow::Result;
 use redis::RedisError;
 use utils::{get_redis_connection, start_redis_server_with_module};
 
 #[test]
 fn test_hello() -> Result<()> {
-    let _guards = vec![start_redis_server_with_module("hello", 6479)?];
-    let mut con = get_redis_connection(6479)?;
+    let _guards = vec![start_redis_server_with_module("hello", 6479)
+        .with_context(|| "failed to start redis server")?];
+    let mut con =
+        get_redis_connection(6479).with_context(|| "failed to connect to redis server")?;
 
-    let res: Vec<i32> = redis::cmd("hello.mul").arg(&[3, 4]).query(&mut con)?;
+    let res: Vec<i32> = redis::cmd("hello.mul")
+        .arg(&[3, 4])
+        .query(&mut con)
+        .with_context(|| "failed to run hello.mul")?;
     assert_eq!(res, vec![3, 4, 12]);
 
     let res: Result<Vec<i32>, RedisError> =
