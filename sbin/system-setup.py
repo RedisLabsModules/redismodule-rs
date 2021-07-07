770d0ce8ad247aeeeddea387abf68a3be636f9d7
Chayim I. Kirshen <c@kirshen.com>
2021-07-07 12:43:41 +0300

    need to force redis install on osx...

--- sbin/system-setup.py
+++ sbin/system-setup.py
@@ -43,6 +43,7 @@ class RedisModuleRSSetup(paella.Setup):
     def macos(self):
         self.install_gnu_utils()
         self.run("%s/bin/getgcc" % READIES)
+        self.run("%s/bin/getredis -v 6" % READIES)
 
     def common_last(self):
         self.pip_install("toml")
