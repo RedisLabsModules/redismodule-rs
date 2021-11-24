8bd942b494b0df7020cc93be8808c245e1db3494
Danni Moiseyev <danni@redislabs.com>
2021-11-24 17:09:52 +0200

    Expose `RedisModule_GetServerInfo` as a rust API (#195)
    * Expose `RedisModule_GetServerInfo` and `RedisModule_GetServerInfo`  as a rust API

---
+++ examples/info.rs
@@ -0,0 +1,31 @@
+#[macro_use]
+extern crate redis_module;
+
+use redis_module::{Context, NextArg, RedisError, RedisResult, RedisString, RedisValue};
+
+fn info_cmd(ctx: &Context, args: Vec<RedisString>) -> RedisResult {
+    if args.len() < 3 {
+        return Err(RedisError::WrongArity);
+    }
+
+    let mut args = args.into_iter().skip(1);
+
+    let section = args.next_str()?;
+    let field = args.next_str()?;
+
+    let server_info = ctx.server_info(section);
+    Ok(server_info
+        .field(field)
+        .map_or(RedisValue::Null, RedisValue::BulkRedisString))
+}
+
+//////////////////////////////////////////////////////
+
+redis_module! {
+    name: "info",
+    version: 1,
+    data_types: [],
+    commands: [
+        ["infoex", info_cmd, "", 0, 0, 0],
+    ],
+}
