08d7dac510e5692abbe5a9d063c2f8c6763f9811
meir <meir@redis.com>
2023-01-22 14:55:06 +0200

    Improve tests

--- examples/call.rs
+++ examples/call.rs
@@ -10,19 +10,14 @@ fn call_test(ctx: &Context, _: Vec<RedisString>) -> RedisResult {
         return Err(RedisError::Str("Failed calling 'ECHO TEST'"));
     }
 
-    let res: Result<String, RedisError> = ctx.call("ECHO", &[b"TEST"])?.into();
-    if "TEST" != &res? {
-        return Err(RedisError::Str("Failed calling 'ECHO TEST' with static [u8]"));
-    }
-
-    let res: Result<String, RedisError> = ctx.call("ECHO", &[&"TEST".to_string()])?.into();
-    if "TEST" != &res? {
-        return Err(RedisError::Str("Failed calling 'ECHO TEST' with String"));
-    }
-
     let res: Result<String, RedisError> = ctx.call("ECHO",vec!["TEST"].as_slice())?.into();
     if "TEST" != &res? {
         return Err(RedisError::Str("Failed calling 'ECHO TEST' dynamic str vec"));
+    }
+
+    let res: Result<String, RedisError> = ctx.call("ECHO", &[b"TEST"])?.into();
+    if "TEST" != &res? {
+        return Err(RedisError::Str("Failed calling 'ECHO TEST' with static [u8]"));
     }
 
     let res: Result<String, RedisError> = ctx.call("ECHO",vec![b"TEST"].as_slice())?.into();
@@ -30,9 +25,24 @@ fn call_test(ctx: &Context, _: Vec<RedisString>) -> RedisResult {
         return Err(RedisError::Str("Failed calling 'ECHO TEST' dynamic &[u8] vec"));
     }
 
+    let res: Result<String, RedisError> = ctx.call("ECHO", &[&"TEST".to_string()])?.into();
+    if "TEST" != &res? {
+        return Err(RedisError::Str("Failed calling 'ECHO TEST' with String"));
+    }
+
+    let res: Result<String, RedisError> = ctx.call("ECHO",vec![&"TEST".to_string()].as_slice())?.into();
+    if "TEST" != &res? {
+        return Err(RedisError::Str("Failed calling 'ECHO TEST' dynamic &[u8] vec"));
+    }
+
     let res: Result<String, RedisError> = ctx.call("ECHO", &[&ctx.create_string("TEST")])?.into();
     if "TEST" != &res? {
         return Err(RedisError::Str("Failed calling 'ECHO TEST' with RedisString"));
+    }
+
+    let res: Result<String, RedisError> = ctx.call("ECHO", vec![&ctx.create_string("TEST")].as_slice())?.into();
+    if "TEST" != &res? {
+        return Err(RedisError::Str("Failed calling 'ECHO TEST' with dynamic array of RedisString"));
     }
 
     Ok("pass".into())
