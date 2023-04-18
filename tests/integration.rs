867f6a49127ff5d72ff59733cc9ca116c7729149
Victor Polevoy <fx@thefx.co>
2023-04-18 08:51:27 +0200

    Revert the accidentally removed tests.

--- tests/integration.rs
+++ tests/integration.rs
@@ -198,6 +198,64 @@ fn test_scan() -> Result<()> {
     Ok(())
 }
 
+#[cfg(not(feature = "experimental-api"))]
+#[cfg_attr(not(feature = "experimental-api"), test)]
+fn test_stream_reader() -> Result<()> {
+    let port: u16 = 6487;
+    let _guards = vec![start_redis_server_with_module("stream", port)
+        .with_context(|| "failed to start redis server")?];
+    let mut con =
+        get_redis_connection(port).with_context(|| "failed to connect to redis server")?;
+
+    let _: String = redis::cmd("XADD")
+        .arg(&["s", "1-1", "foo", "bar"])
+        .query(&mut con)
+        .with_context(|| "failed to add data to the stream")?;
+
+    let _: String = redis::cmd("XADD")
+        .arg(&["s", "1-2", "foo", "bar"])
+        .query(&mut con)
+        .with_context(|| "failed to add data to the stream")?;
+
+    let res: String = redis::cmd("STREAM_POP")
+        .arg(&["s"])
+        .query(&mut con)
+        .with_context(|| "failed to run keys_pos")?;
+    assert_eq!(res, "1-1");
+
+    let res: String = redis::cmd("STREAM_POP")
+        .arg(&["s"])
+        .query(&mut con)
+        .with_context(|| "failed to run keys_pos")?;
+    assert_eq!(res, "1-2");
+
+    let res: usize = redis::cmd("XLEN")
+        .arg(&["s"])
+        .query(&mut con)
+        .with_context(|| "failed to add data to the stream")?;
+
+    assert_eq!(res, 0);
+
+    Ok(())
+}
+
+#[test]
+fn test_call() -> Result<()> {
+    let port: u16 = 6488;
+    let _guards = vec![start_redis_server_with_module("call", port)
+        .with_context(|| "failed to start redis server")?];
+    let mut con =
+        get_redis_connection(port).with_context(|| "failed to connect to redis server")?;
+
+    let res: String = redis::cmd("call.test")
+        .query(&mut con)
+        .with_context(|| "failed to run string.set")?;
+
+    assert_eq!(&res, "pass");
+
+    Ok(())
+}
+
 #[test]
 fn test_ctx_flags() -> Result<()> {
     let port: u16 = 6489;
