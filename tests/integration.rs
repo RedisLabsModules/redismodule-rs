bbe6abccb947fd41e0247bfa53834757a8b17218
Victor Polevoy <fx@thefx.co>
2023-04-18 11:17:08 +0200

    Fix the tests.

--- tests/integration.rs
+++ tests/integration.rs
@@ -151,9 +151,8 @@ fn test_test_helper_err() -> Result<()> {
     Ok(())
 }
 
-#[cfg(feature = "experimental-api")]
-#[cfg_attr(feature = "experimental-api", test)]
-fn test_stream_reader() -> Result<()> {
+#[test]
+fn test_string() -> Result<()> {
     let port: u16 = 6485;
     let _guards = vec![start_redis_server_with_module("string", port)
         .with_context(|| "failed to start redis server")?];
@@ -198,8 +197,7 @@ fn test_scan() -> Result<()> {
     Ok(())
 }
 
-#[cfg(not(feature = "experimental-api"))]
-#[cfg_attr(not(feature = "experimental-api"), test)]
+#[test]
 fn test_stream_reader() -> Result<()> {
     let port: u16 = 6487;
     let _guards = vec![start_redis_server_with_module("stream", port)
