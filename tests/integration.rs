fc51c7e5312c1572b95f68c93e9288e88e21846c
Ephraim Feldblum <ephraim.feldblum@redis.com>
2024-11-04 16:13:42 +0200

    using `TestConnection` API

--- tests/integration.rs
+++ tests/integration.rs
@@ -278,11 +278,7 @@ fn test_get_current_user() -> Result<()> {
 #[test]
 #[cfg(feature = "min-redis-compatibility-version-7-4")]
 fn test_set_acl_categories() -> Result<()> {
-    let port: u16 = 6490;
-    let _guards = vec![start_redis_server_with_module("acl", port)
-        .with_context(|| "failed to start redis server")?];
-    let mut con =
-        get_redis_connection(port).with_context(|| "failed to connect to redis server")?;
+    let mut con = TestConnection::new("acl");
 
     let res: Vec<String> = redis::cmd("ACL").arg("CAT").query(&mut con)?;
     assert!(res.contains(&"acl".to_owned()));
@@ -293,11 +289,7 @@ fn test_set_acl_categories() -> Result<()> {
 #[test]
 #[cfg(feature = "min-redis-compatibility-version-8-0")]
 fn test_set_acl_categories_commands() -> Result<()> {
-    let port: u16 = 6490;
-    let _guards = vec![start_redis_server_with_module("acl", port)
-        .with_context(|| "failed to start redis server")?];
-    let mut con =
-        get_redis_connection(port).with_context(|| "failed to connect to redis server")?;
+    let mut con = TestConnection::new("acl");
 
     let res: Vec<String> = redis::cmd("ACL").arg("CAT").arg("acl").query(&mut con)?;
     assert!(
