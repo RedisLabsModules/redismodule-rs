ef2667cda7e4043312d2c8975912e83d0e098ef7
Jonas Kruckenberg <iterpre@protonmail.com>
2026-05-19 09:21:20 +0200

    implement std::error::Error for RedisError and cleanup

--- src/rediserror.rs
+++ src/rediserror.rs
@@ -1,14 +1,73 @@
 use crate::context::call_reply::{ErrorCallReply, ErrorReply};
 pub use crate::raw;
+use std::borrow::Cow;
 use std::ffi::CStr;
 use std::fmt;
 
 #[derive(Debug)]
+#[non_exhaustive]
 pub enum RedisError {
     WrongArity,
     Str(&'static str),
     String(String),
     WrongType,
+    ContainsNul(std::ffi::NulError),
+    InvalidUtf8(std::str::Utf8Error),
+}
+
+impl RedisError {
+    #[must_use]
+    pub const fn nonexistent_key() -> Self {
+        Self::Str("ERR could not perform this operation on a key that doesn't exist")
+    }
+
+    #[must_use]
+    pub const fn short_read() -> Self {
+        Self::Str("ERR short read or OOM loading DB")
+    }
+
+    pub(crate) fn to_str(&self) -> Cow<'_, str> {
+        match self {
+            RedisError::WrongArity => Cow::Borrowed("Wrong Arity"),
+            RedisError::Str(str) => Cow::Borrowed(str),
+            RedisError::String(str) => Cow::Borrowed(str.as_str()),
+            RedisError::WrongType => {
+                const ERR_MSG: &str = {
+                    let Ok(str) = CStr::from_bytes_with_nul(raw::REDISMODULE_ERRORMSG_WRONGTYPE)
+                    else {
+                        panic!()
+                    };
+                    let Ok(str) = std::str::from_utf8(str.to_bytes()) else {
+                        panic!()
+                    };
+
+                    str
+                };
+
+                Cow::Borrowed(ERR_MSG)
+            }
+            RedisError::ContainsNul(err) => {
+                Cow::Owned(format!("String contained interior NUL byte: {err}"))
+            }
+            RedisError::InvalidUtf8(err) => Cow::Owned(format!("Invalid UTF8: {err}")),
+        }
+    }
+}
+
+impl fmt::Display for RedisError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(&self.to_str())
+    }
+}
+
+impl std::error::Error for RedisError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            RedisError::ContainsNul(err) => Some(err),
+            RedisError::InvalidUtf8(err) => Some(err),
+            _ => None,
+        }
+    }
 }
 
 impl<'root> From<ErrorCallReply<'root>> for RedisError {
@@ -29,41 +88,8 @@ impl<'root> From<ErrorReply<'root>> for RedisError {
     }
 }
 
-impl RedisError {
-    #[must_use]
-    pub const fn nonexistent_key() -> Self {
-        Self::Str("ERR could not perform this operation on a key that doesn't exist")
-    }
-
-    #[must_use]
-    pub const fn short_read() -> Self {
-        Self::Str("ERR short read or OOM loading DB")
-    }
-}
-
-impl<T: std::error::Error> From<T> for RedisError {
-    fn from(e: T) -> Self {
-        Self::String(format!("ERR {e}"))
-    }
-}
-
-impl fmt::Display for RedisError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let d = match self {
-            Self::WrongArity => "Wrong Arity",
-            // remove NUL from the end of raw::REDISMODULE_ERRORMSG_WRONGTYPE
-            // before converting &[u8] to &str to ensure CString::new() doesn't
-            // panic when this is passed to it.
-            Self::WrongType => std::str::from_utf8(
-                CStr::from_bytes_with_nul(raw::REDISMODULE_ERRORMSG_WRONGTYPE)
-                    .unwrap()
-                    .to_bytes(),
-            )
-            .unwrap(),
-            Self::Str(s) => s,
-            Self::String(s) => s.as_str(),
-        };
-
-        write!(f, "{d}")
+impl From<std::str::Utf8Error> for RedisError {
+    fn from(err: std::str::Utf8Error) -> Self {
+        RedisError::InvalidUtf8(err)
     }
 }
